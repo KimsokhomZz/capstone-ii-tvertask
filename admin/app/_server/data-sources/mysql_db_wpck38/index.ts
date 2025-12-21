8a34ea1fba070940d0138251b2d3494cf381b51e
iamkaizen <kimsokhom654@gmail.com>
2025-12-21 23:24:33 +0700

    fix admin dashboard

--- admin/app/_server/data-sources/mysql_db_wpck38/index.ts
+++ admin/app/_server/data-sources/mysql_db_wpck38/index.ts
@@ -5,8 +5,14 @@ import knex from 'knex';
  * Learn more at https://knexjs.org/guide/#configuration-options
  */
 const client = knex({
-  client: "mysql2",
-  connection: process.env.DATABASE_CONNECTION,
+  client: 'mysql2',
+  connection: {
+    host: 'localhost',
+    port: 3306,
+    user: 'root',
+    password: 'password',
+    database: 'tvertask_db',
+  },
 });
 
 export default new KnexMysql2Adapter(client);
