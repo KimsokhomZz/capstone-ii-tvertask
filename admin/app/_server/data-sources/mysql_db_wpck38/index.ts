49b78cf819ede3b38a34e350efe45d25a5953433
SamnangKong426 <samnangkong499@gmail.com>
2025-12-21 20:45:21 +0700

    fix: change service url

--- admin/app/_server/data-sources/mysql_db_wpck38/index.ts
+++ admin/app/_server/data-sources/mysql_db_wpck38/index.ts
@@ -5,14 +5,8 @@ import knex from 'knex';
  * Learn more at https://knexjs.org/guide/#configuration-options
  */
 const client = knex({
-  client: 'mysql2',
-  connection: {
-    host: 'localhost',
-    port: 3306,
-    user: 'root',
-    password: 'password',
-    database: 'tvertask_db',
-  },
+  client: "mysql2",
+  connection: process.env.DATABASE_CONNECTION,
 });
 
 export default new KnexMysql2Adapter(client);
